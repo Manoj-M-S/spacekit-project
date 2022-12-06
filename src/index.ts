2d499205248d5fecf79a988e95864c6f29369b79
gokul1630 <gokulgp3167@gmail.com>
2022-12-06 12:00:01 +0530

    Added AnchorTab

--- src/index.ts
+++ src/index.ts
@@ -10,3 +10,4 @@ export * from './Components/01-stars/Radio';
 export * from './Components/01-stars/Checkbox';
 export * from './Components/02-Constellations/Progress';
 export * from './Components/01-stars/Tabs';
+export * from './Components/01-stars/AnchorTabs';
