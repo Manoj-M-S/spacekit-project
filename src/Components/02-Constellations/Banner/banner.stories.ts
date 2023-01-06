59b16ae8e2d09bf9c0c92b2ad6407f1deee00545
Shubham <shubham.kumar@qed42.com>
2023-01-06 16:53:48 +0530

    Added notification banner

---
+++ src/Components/02-Constellations/Banner/banner.stories.ts
@@ -0,0 +1,62 @@
+import { Meta } from '@storybook/web-components';
+import { html } from 'lit';
+import './src/NotificationBanner';
+
+export default {
+  title: 'Constellations/Banner',
+  component: 'space-notification',
+  argTypes: {
+    background: {
+      options: ['primaryDark', 'neutralMedium', 'neutralExtraDark'],
+      control: {
+        type: 'radio',
+      },
+    },
+    size: {
+      options: ['small', 'large'],
+      control: {
+        type: 'radio',
+      },
+    },
+  },
+} as Meta;
+
+const Template = ({
+  background,
+  linkText,
+  linkHref,
+  title,
+  showIcon,
+  iconName,
+  size,
+}: any) => {
+  return html`
+    <space-notification
+      background=${background}
+      link-href=${linkHref}
+      link-text=${linkText}
+      title=${title}
+      ?show-icon=${showIcon}
+      icon-name=${iconName}
+      size=${size}
+    >
+    </space-notification>
+  `;
+};
+
+export const Default = Template.bind({});
+export const Icon = Template.bind({});
+
+Default.args = {
+  background: 'primaryDark',
+  linkHref: '#',
+  title: 'This page is now available in 12 languages',
+  linkText: 'Choose a different language',
+  size: 'small',
+};
+
+Icon.args = {
+  ...Default.args,
+  showIcon: true,
+  iconName: 'globe',
+};
---
+++ src/Components/02-Constellations/Banner/src/NotificationBanner.ts
@@ -0,0 +1,110 @@
+import { html, LitElement } from 'lit';
+import { customElement, property, state } from 'lit/decorators.js';
+import { classMap } from 'lit/directives/class-map.js';
+import '../../../01-stars/button/src/Button';
+import styles from './NotificationBanner.scss.lit';
+import '../../../01-stars/Link/src/Link';
+
+export type Background = 'primaryDark' | 'neutralMedium' | 'neutralExtraDark';
+export type Size = 'small' | 'large';
+
+@customElement('space-notification')
+export default class NotificationBanner extends LitElement {
+  static styles = [styles];
+
+  /** @attr show-icon */
+  @property({ type: Boolean, attribute: 'show-icon' })
+  showIcon: boolean = false;
+
+  /** @attr icon-name */
+  @property({ type: String, attribute: 'icon-name' })
+  iconName: string = '';
+
+  /** @attr background */
+  @property({ type: String, attribute: 'background' })
+  background: Background = 'primaryDark';
+
+  /** @attr size */
+  @property({ type: String, attribute: 'size' })
+  size: Size = 'small';
+
+  /** @attr title */
+  @property({ type: String, attribute: 'title' })
+  title: string = '';
+
+  /** @attr link-text */
+  @property({ type: String, attribute: 'link-text' })
+  linkText: string = '';
+
+  /** @attr link-href */
+  @property({ type: String, attribute: 'link-href' })
+  linkHref: string = '';
+
+  @state()
+  onClick!: () => void;
+
+  renderLinkIcon() {
+    const iconClass = {
+      'link-icon': true,
+      'right': true,
+      [`icon-${this.background}`]: this.background,
+    };
+    return html` <space-icon
+      class=${classMap(iconClass)}
+      icon-name="arrowRight"
+    ></space-icon>`;
+  }
+
+  render() {
+    const bannerClass = {
+      'notification-banner': true,
+      [`background-bg-${this.background}`]: this.background,
+    };
+
+    const iconClass = {
+      icon: true,
+      [`icon-${this.background}`]: this.background,
+    };
+
+    const titleClass = {
+      title: true,
+      [`title-${this.background}`]: this.background,
+    };
+
+    const linkClass = {
+      link: true,
+      [`link-${this.background}`]: this.background,
+    };
+
+    const bannerSize = {
+      [`banner-${this.size}`]: this.size,
+    };
+    return html`
+      <div class=${classMap(bannerClass)}>
+        <div class=${classMap(bannerSize)}>
+          <div class="main">
+            ${this.showIcon
+              ? html`<space-icon
+                  icon-name="globe"
+                  class=${classMap(iconClass)}
+                ></space-icon>`
+              : null}
+            <div class=${classMap(titleClass)}>${this.title}</div>
+          </div>
+          <a class=${classMap(linkClass)} href=${this.linkHref}
+            >${this.linkText} ${this.renderLinkIcon()}</a
+          >
+        </div>
+        <a href="#" @click=${this.onClick}
+          ><space-icon class=${classMap(iconClass)} icon-name="x"></space-icon
+        ></a>
+      </div>
+    `;
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'space-notification': NotificationBanner;
+  }
+}
