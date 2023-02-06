dab8470674e3f133ea8410b797d7d20f64bbf0f0
Shubham <shubham.kumar@qed42.com>
2023-02-06 19:22:55 +0530

    Added Footer with All Variation

--- src/Components/03-Galaxy/Footer/footer.stories.ts
+++ src/Components/03-Galaxy/Footer/footer.stories.ts
@@ -9,6 +9,14 @@ import './src/TypeSix/TypeSix';
 import './src/TypeSeven/TypeSeven';
 import './src/TypeEight/TypeEight';
 import './src/TypeNine/TypeNine';
+import './src/TypeTen/TypeTen';
+import './src/TypeEleven/TypeEleven';
+import './src/TypeTwelve/TypeTwelve';
+import './src/TypeThirteen/TypeThirteen';
+import './src/TypeFourteen/TypeFourteen';
+import './src/TypeFifteen/TypeFifteen';
+import './src/TypeSixteen/TypeSixteen';
+import './src/TypeSeventeen/TypeSeventeen';
 
 export default {
   title: 'Galaxy/Footer',
@@ -216,6 +224,10 @@ const locationOptions = [
 ];
 
 const quickLinks = [
+  {
+    navigationText: 'Overview',
+    navigationHref: '#',
+  },
   {
     navigationText: 'Overview',
     navigationHref: '#',
@@ -452,7 +464,6 @@ const typeNine = ({
   logoSrc,
   logoAlt,
   supportiveText,
-  linkOptions,
   footerLinkOptions,
   inputLabel,
   inputPlaceholder,
@@ -461,7 +472,6 @@ const typeNine = ({
 }: any) => {
   return html`
     <space-footer-type-nine
-      .linkOptions=${linkOptions}
       ?background=${background}
       footer-text=${footerText}
       logo-src=${logoSrc}
@@ -477,6 +487,208 @@ const typeNine = ({
   `;
 };
 
+const typeTen = ({
+  background,
+  footerText,
+  logoSrc,
+  logoAlt,
+  supportiveText,
+  inputLabel,
+  inputPlaceholder,
+  btnLabel,
+  quickLinks,
+  socialLinkOptions,
+  iosLogoSrc,
+  androidLogoSrc,
+}: any) => {
+  return html`
+    <space-footer-type-ten
+      ?background=${background}
+      footer-text=${footerText}
+      logo-src=${logoSrc}
+      logo-alt=${logoAlt}
+      supportive-text=${supportiveText}
+      input-label=${inputLabel}
+      input-placeholder=${inputPlaceholder}
+      btn-label=${btnLabel}
+      .quickLinks=${quickLinks}
+      .socialLinkOptions=${socialLinkOptions}
+      ios-logo-src=${iosLogoSrc}
+      android-logo-src=${androidLogoSrc}
+    >
+    </space-footer-type-ten>
+  `;
+};
+
+const typeEleven = ({
+  background,
+  footerText,
+  linkOptions,
+  socialLinkOptions,
+  footerLinkOptions,
+  title,
+  quickLinks,
+  supportiveText,
+}: any) => {
+  return html`
+    <space-footer-type-eleven
+      .linkOptions=${linkOptions}
+      ?background=${background}
+      footer-text=${footerText}
+      .socialLinkOptions=${socialLinkOptions}
+      .footerLinkOptions=${footerLinkOptions}
+      title=${title}
+      .quickLinks=${quickLinks}
+      supportive-text=${supportiveText}
+    >
+    </space-footer-type-eleven>
+  `;
+};
+
+const typeTwelve = ({
+  background,
+  footerText,
+  linkOptions,
+  socialLinkOptions,
+  footerLinkOptions,
+  title,
+  supportiveText,
+  logoSrc,
+  logoAlt,
+  inputLabel,
+  inputPlaceholder,
+  btnLabel,
+}: any) => {
+  return html`
+    <space-footer-type-twelve
+      .linkOptions=${linkOptions}
+      ?background=${background}
+      footer-text=${footerText}
+      .socialLinkOptions=${socialLinkOptions}
+      .footerLinkOptions=${footerLinkOptions}
+      title=${title}
+      supportive-text=${supportiveText}
+      logo-alt=${logoAlt}
+      logo-src=${logoSrc}
+      input-label=${inputLabel}
+      input-placeholder=${inputPlaceholder}
+      btn-label=${btnLabel}
+    >
+    </space-footer-type-twelve>
+  `;
+};
+
+const typeThirteen = ({
+  background,
+  footerText,
+  logoSrc,
+  logoAlt,
+  footerLinkOptions,
+  quickLinks,
+}: any) => {
+  return html`
+    <space-footer-type-thirteen
+      ?background=${background}
+      footer-text=${footerText}
+      logo-src=${logoSrc}
+      logo-alt=${logoAlt}
+      .footerLinkOptions=${footerLinkOptions}
+      .quickLinks=${quickLinks}
+    >
+    </space-footer-type-thirteen>
+  `;
+};
+
+const typeFourteen = ({
+  background,
+  footerText,
+  logoSrc,
+  logoAlt,
+  quickLinks,
+  inputLabel,
+  inputPlaceholder,
+  btnLabel,
+}: any) => {
+  return html`
+    <space-footer-type-fourteen
+      ?background=${background}
+      footer-text=${footerText}
+      logo-src=${logoSrc}
+      logo-alt=${logoAlt}
+      .quickLinks=${quickLinks}
+      input-label=${inputLabel}
+      input-placeholder=${inputPlaceholder}
+      btn-label=${btnLabel}
+    >
+    </space-footer-type-fourteen>
+  `;
+};
+
+const typeFifteen = ({
+  background,
+  footerText,
+  logoSrc,
+  logoAlt,
+  quickLinks,
+}: any) => {
+  return html`
+    <space-footer-type-fifteen
+      ?background=${background}
+      footer-text=${footerText}
+      logo-src=${logoSrc}
+      logo-alt=${logoAlt}
+      .quickLinks=${quickLinks}
+    >
+    </space-footer-type-fifteen>
+  `;
+};
+
+const typeSixteen = ({
+  background,
+  footerText,
+  logoSrc,
+  logoAlt,
+  socialLinkOptions,
+}: any) => {
+  return html`
+    <space-footer-type-sixteen
+      ?background=${background}
+      footer-text=${footerText}
+      logo-src=${logoSrc}
+      logo-alt=${logoAlt}
+      .socialLinkOptions=${socialLinkOptions}
+    >
+    </space-footer-type-sixteen>
+  `;
+};
+
+const typeSeventeen = ({
+  background,
+  footerText,
+  logoSrc,
+  logoAlt,
+  footerLinkOptions,
+  title,
+  supportiveText,
+  primaryBtnTxt,
+  secondaryBtnTxt,
+}: any) => {
+  return html`
+    <space-footer-type-seventeen
+      ?background=${background}
+      footer-text=${footerText}
+      logo-src=${logoSrc}
+      logo-alt=${logoAlt}
+      .footerLinkOptions=${footerLinkOptions}
+      title=${title}
+      supportive-text=${supportiveText}
+      primary-btn-text=${primaryBtnTxt}
+      secondary-btn-text=${secondaryBtnTxt}
+    >
+    </space-footer-type-seventeen>
+  `;
+};
+
 export const TypeOne = Template.bind({});
 export const TypeTwo = typeTwo.bind({});
 export const TypeThree = typeThree.bind({});
@@ -486,6 +698,14 @@ export const TypeSix = typeSix.bind({});
 export const TypeSeven = typeSeven.bind({});
 export const TypeEight = typeEight.bind({});
 export const TypeNine = typeNine.bind({});
+export const TypeTen = typeTen.bind({});
+export const TypeEleven = typeEleven.bind({});
+export const TypeTwelve = typeTwelve.bind({});
+export const TypeThirteen = typeThirteen.bind({});
+export const TypeFourteen = typeFourteen.bind({});
+export const TypeFifteen = typeFifteen.bind({});
+export const TypeSixteen = typeSixteen.bind({});
+export const TypeSeventeen = typeSeventeen.bind({});
 
 TypeOne.args = {
   linkOptions,
@@ -584,7 +804,6 @@ TypeEight.args = {
 };
 
 TypeNine.args = {
-  linkOptions,
   background: false,
   footerText: '© 2021 SPACE. All rights reserved.',
   supportiveText:
@@ -596,4 +815,96 @@ TypeNine.args = {
   inputLabel: 'Stay upto date',
   btnLabel: 'Subscribe',
   quickLinks,
+};
+
+TypeTen.args = {
+  background: false,
+  footerText: '© 2021 SPACE. All rights reserved.',
+  supportiveText:
+    'Design amazing digital experiences that create more happy in the world.',
+  logoSrc: 'https://i.ibb.co/m8QfWzK/logo.png',
+  logoAlt: 'logo',
+  inputPlaceholder: 'Enter your email',
+  inputLabel: 'Stay upto date',
+  btnLabel: 'Subscribe',
+  quickLinks,
+  socialLinkOptions,
+  iosLogoSrc: 'https://imgtr.ee/images/2023/02/01/MGUFD.png',
+  androidLogoSrc: 'https://imgtr.ee/images/2023/02/01/MG96s.png',
+};
+
+TypeEleven.args = {
+  linkOptions,
+  background: false,
+  footerText: '© 2021 SPACE. All rights reserved.',
+  socialLinkOptions,
+  footerLinkOptions,
+  title: "Let's make something great together.",
+  quickLinks,
+  supportiveText: 'Be the brand that customers want and competitors envy.',
+};
+
+TypeTwelve.args = {
+  linkOptions,
+  background: false,
+  footerText: '© 2021 SPACE. All rights reserved.',
+  socialLinkOptions,
+  footerLinkOptions,
+  title: 'Lorem ipsum dolor sit amet, nsectetur adipiscing elit.',
+  supportiveText:
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Risus dis dictumst amet cursus enim ultricies fames. Pulvinar amet, sed tristique auctor placerat velit a felis. Et pellentesque et aliquam eu urna. Dolor urna, dignissim consectetur morbi nulla. Turpis neque sed a risus ac nec et arcu.',
+  logoSrc: 'https://i.ibb.co/m8QfWzK/logo.png',
+  logoAlt: 'logo',
+  inputPlaceholder: 'Enter your email',
+  inputLabel: 'Stay upto date',
+  btnLabel: 'Subscribe',
+};
+
+TypeThirteen.args = {
+  background: false,
+  footerText: '© 2021 SPACE. All rights reserved.',
+  logoSrc: 'https://i.ibb.co/m8QfWzK/logo.png',
+  logoAlt: 'logo',
+  footerLinkOptions,
+  quickLinks,
+};
+
+TypeFourteen.args = {
+  background: false,
+  footerText: '© 2021 SPACE. All rights reserved.',
+  logoSrc: 'https://i.ibb.co/m8QfWzK/logo.png',
+  logoAlt: 'logo',
+  footerLinkOptions,
+  quickLinks,
+  inputPlaceholder: 'Enter your email',
+  inputLabel: 'Stay upto date',
+  btnLabel: 'Subscribe',
+};
+
+TypeFifteen.args = {
+  background: false,
+  footerText: '© 2021 SPACE',
+  logoSrc: 'https://i.ibb.co/m8QfWzK/logo.png',
+  logoAlt: 'logo',
+  quickLinks,
+};
+
+TypeSixteen.args = {
+  background: false,
+  footerText: '© 2021 SPACE',
+  logoSrc: 'https://i.ibb.co/m8QfWzK/logo.png',
+  logoAlt: 'logo',
+  socialLinkOptions,
+};
+
+TypeSeventeen.args = {
+  background: false,
+  footerText: '© 2021 SPACE. All rights reserved.',
+  logoSrc: 'https://i.ibb.co/QPS2rCY/Logomark.png',
+  logoAlt: 'logo',
+  footerLinkOptions,
+  title: 'Join our newsletter',
+  supportiveText: 'We’ll send you a nice letter once per week. No spam.',
+  primaryBtnTxt: 'View demo',
+  secondaryBtnTxt: 'Get started',
 };
