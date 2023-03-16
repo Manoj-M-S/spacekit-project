9cdfabba03058e654fad5fed318060f37acce92c
Shubham <shubham.kumar@qed42.com>
2023-03-16 17:00:44 +0530

    Added Signup

--- src/index.ts
+++ src/index.ts
@@ -52,6 +52,8 @@ export * from './Components/03-Galaxy/Footer';
 export * from './Components/03-Galaxy/HeroBanner';
 export * from './Components/03-Galaxy/ContactHeader';
 export * from './Components/03-Galaxy/ContactSection';
+export * from './Components/03-Galaxy/Login';
+export * from './Components/03-Galaxy/Signup';
 
 // ------ LANDING PAGES ------ //
 export * from './Components/04-Pages/LandingPages';
